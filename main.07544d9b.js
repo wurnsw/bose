ddcaad6dde45f97e1e0b7fe94f3441bd5d9e05f3
wurnsw <wurnsw@gmail.com>
2024-12-02 00:19:57 +0200

    Updates

--- main.07544d9b.js
+++ main.07544d9b.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52758" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63534" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
